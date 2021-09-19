f291637f994f699903efcd619c01c7e51f6a597d
naima <naimamohamed19@hotmail.com>
2021-09-19 16:41:56 -0400

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
 @app.route('/')
-def hello_world():
+def main():
     ARTIST_IDS = ["78rUTD7y6Cy67W1RVzYs7t", "2xvtxDNInKDV4AvGmjw6d1", 
     "1CbA4z6JauNQnHzOErDQL6", "3x2XRFCUMHeXZ9uRit3pKu", 
     "2SJhf6rTOU53g8yBdAjPby", "73sIBHcqh3Z3NyqHKZ7FOL",
